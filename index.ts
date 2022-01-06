42ffba297ee5e3aa85330179d62f0181b61a6d70
Gjergj Kadriu <gjergjk71@gmail.com>
2022-01-07 00:15:29 +0100

    push

--- index.ts
+++ index.ts
@@ -3,10 +3,7 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  const post = await prisma.post.update({
-    where: { id: 1 },
-    data: { published: true },
-  })
+  const post = await prisma.post.findMany()
   console.log(post)
 }
 
